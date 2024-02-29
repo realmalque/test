361228d028a32f33b490ede63c4c4c106538ce77
realmalque <65733218+realmalque@users.noreply.github.com>
2024-02-29 22:49:26 +0200

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,7 +1,11 @@
-sdads
-sdac
-msmdaksn
-ModuleNotFoundError
-dsd
-dsd
-sdasd
+# n = 178871
+# days = n//(24*60*60)
+# hours = (n -days*24*60*60) // (60 * 60)
+# minutes = (n - days*24*60*60-hours*60*60) // 60
+# seconds = n -days*24*60*60 - hours * 60 * 60 - minutes * 60
+
+# print(f"{days}:{hours}:{minutes}:{seconds}")
+
+ratio = 10
+result = 8 * (ratio + 5) - ratio ** 2
+print(result)
